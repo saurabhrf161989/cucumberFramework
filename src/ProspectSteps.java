79cb098aef4a02c41e1e5381e33b811f11d9e8dd
Saurabh Gupta <saurabhrf161989@gmail.com>
2021-10-05 22:42:45 +0530

    update from local

--- src/ProspectSteps.java
+++ src/ProspectSteps.java
@@ -151,6 +151,17 @@ public class ProspectSteps {
 				
 	}
 
+	@Then("^user should land on business info page$")
+	public void user_should_land_on_shopping_page() throws Throwable {
+		
+		Boolean a=driver.findElement(By.xpath("//h1[text()='business Cart']")).isDisplayed();
+		
+		
+		 Thread.sleep(3000);
+				
+	}
+
+
 	
 	@After
 	public void tearDown(Scenario scenario) throws InterruptedException, IOException{
