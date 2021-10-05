d733acc9b49a3dff4db3b1f1ccad34149f50db8a
saurabhrf161989 <83653314+saurabhrf161989@users.noreply.github.com>
2021-10-05 23:04:50 +0530

    Update ProspectSteps.java

--- src/ProspectSteps.java
+++ src/ProspectSteps.java
@@ -142,15 +142,7 @@ public class ProspectSteps {
 				
 	}
 
-	@Then("^user should land on review page$")
-	public void user_should_land_on_shopping_page() throws Throwable {
-		
-		Boolean a=driver.findElement(By.xpath("//h1[text()='review Cart']")).isDisplayed();
-		
-		
-		 Thread.sleep(3000);
-				
-	}
+	
 
 
 	
